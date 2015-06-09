1d8977e9b68fe4903c45e0aafd003ceccd6aa3ec
davidpodhola <david.podhola@naseukoly.cz>
2015-06-09 13:46:12 +0200

    remoting not working as:
    - WebSharper uses POST see http://websharper.com/docs/remoting
    - SharePointContext does not allow POST see http://stackoverflow.com/questions/23828750/sharepoint-2013-mvc-5-provider-hosted-app-fails-to-authenticate-on-httppost-usi

---
+++ EmptyWebSharperSharePointAppWeb/Controllers/ClientWebPart1Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmptyWebSharperSharePointAppWeb.Controllers
+{
+    public class ClientWebPart1Controller : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
