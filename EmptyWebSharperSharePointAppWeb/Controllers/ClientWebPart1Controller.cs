d9cf92cd025be5601b003be6fd32406f465f9bfc
davidpodhola <david.podhola@naseukoly.cz>
2015-06-10 09:41:38 +0200

    Index2 added

--- EmptyWebSharperSharePointAppWeb/Controllers/ClientWebPart1Controller.cs
+++ EmptyWebSharperSharePointAppWeb/Controllers/ClientWebPart1Controller.cs
@@ -12,5 +12,9 @@ namespace EmptyWebSharperSharePointAppWeb.Controllers
         {
             return View();
         }
+        public ActionResult Index2()
+        {
+            return View();
+        }
     }
 }
