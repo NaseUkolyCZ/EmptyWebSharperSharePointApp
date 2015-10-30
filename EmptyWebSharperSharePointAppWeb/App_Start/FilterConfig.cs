93a91fe7015415d2ae39cf6556547e3511af4fe0
davidpodhola <david.podhola@naseukoly.cz>
2015-10-30 22:18:54 +0100

    reverting back to original WebSharper.AspNetMvc nuget
    you can NOT have
    
    filters.Add(new HandleErrorAttribute());
    
    in RegisterGlobalFilters

--- EmptyWebSharperSharePointAppWeb/App_Start/FilterConfig.cs
+++ EmptyWebSharperSharePointAppWeb/App_Start/FilterConfig.cs
@@ -7,8 +7,6 @@ namespace EmptyWebSharperSharePointAppWeb
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
-
             filters.Add(new WebSharper.AspNetMvc.Filter());
         }
     }
