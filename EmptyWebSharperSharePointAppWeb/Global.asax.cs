de7476719e9f4ac8f39370f35507ae369f807b9d
davidpodhola <david.podhola@naseukoly.cz>
2015-10-31 16:50:16 +0100

    WIP

--- EmptyWebSharperSharePointAppWeb/Global.asax.cs
+++ EmptyWebSharperSharePointAppWeb/Global.asax.cs
@@ -14,8 +14,6 @@ namespace EmptyWebSharperSharePointAppWeb
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
