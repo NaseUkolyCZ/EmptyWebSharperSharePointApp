00704a1a340cdc3d1f49fab3c324a016864d9387
davidpodhola <david.podhola@naseukoly.cz>
2015-10-31 16:07:26 +0100

    minimized version working
    do not forget to remove routes.MapRoute, if you do not want to duplicate the controller/views in C# project ;-)

--- EmptyWebSharperSharePointAppWeb/App_Start/RouteConfig.cs
+++ EmptyWebSharperSharePointAppWeb/App_Start/RouteConfig.cs
@@ -11,13 +11,6 @@ namespace EmptyWebSharperSharePointAppWeb
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
         }
     }
 }
