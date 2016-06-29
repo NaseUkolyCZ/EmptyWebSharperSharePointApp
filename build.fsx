87be756c8e6461f3e6c5785fe57514e889312047
David Podhola <david.podhola@naseukoly.cz>
2016-06-29 15:53:11 +0200

    Lint

--- build.fsx
+++ build.fsx
@@ -1,11 +1,23 @@
 #r "packages/FAKE/tools/FakeLib.dll"
+#r "packages/FSharpLint/FSharpLint.FAKE.dll"
+
 open Fake
+
+open FSharpLint.FAKE
+
+Target "Lint" (fun _ ->
+    !! "src/**/*.fsproj"
+        |> Seq.iter (FSharpLint id)
+)
 
 Target "BuildApp" (fun _ ->
   !! "src/**/*.?sproj"
   |> MSBuildRelease "" "Build"
   |> Log "AppBuild-Output: "
 )
+
+"Lint"            
+   ==> "BuildApp"
 
 // start build
 RunTargetOrDefault "BuildApp"
